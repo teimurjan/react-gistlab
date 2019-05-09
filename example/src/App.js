7a21565a937102a8477313899586011d53b38ebb
Teimur Gasanov <teymurgg321@gmail.com>
2019-05-10 00:14:50 +0600

    Add example app

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { GithubGist } from '../../lib/index';
+
+function App() {
+  return (
+    <div className="App">
+      <GithubGist url="https://gist.github.com/JamieFarrelly/60f18768e48452f2e7ff498b5271efcc#file-constants-java" />
+    </div>
+  );
+}
+
+export default App;
