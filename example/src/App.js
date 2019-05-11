3cd83fd79aa3ce51b4f8b4dbb6befb77e36f3aa5
Teimur Gasanov <teymurgg321@gmail.com>
2019-05-11 08:27:13 +0600

    Add example

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import { GithubGist } from '../../lib/index';
+import { GithubGist } from "@lib/index";
 
 function App() {
   return (
     <div className="App">
-      <GithubGist url="https://gist.github.com/JamieFarrelly/60f18768e48452f2e7ff498b5271efcc#file-constants-java" />
+      <GithubGist
+        url="https://gist.github.com/JamieFarrelly/60f18768e48452f2e7ff498b5271efcc#file-constants-java"
+        Loading={() => "Loading..."}
+      />
+      <GithubGist
+        url="https://gist.github.com/JamieFarrelly/60f18768e48452f2e7ff498b5271efcc"
+        Loading={() => "Loading..."}
+      />
     </div>
   );
 }
